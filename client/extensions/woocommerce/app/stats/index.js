717c9cdc6c0b79842c32a66820150e22a760cdd0
Paul Sealock <psealock@gmail.com>
2017-05-22 14:23:41 +1200

    Woo Stats: Normalize data

--- client/extensions/woocommerce/app/stats/index.js
+++ client/extensions/woocommerce/app/stats/index.js
@@ -2,19 +2,47 @@
  * External dependencies
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
  */
 import Main from 'components/main';
 import StatsNavigation from './stats-navigation';
+import QuerySiteStats from 'components/data/query-site-stats';
+import { getSelectedSiteId, getSelectedSiteSlug } from 'state/ui/selectors';
 
-export default class Stats extends Component {
+class Stats extends Component {
+	createQuery( unit ) {
+		return {
+			unit,
+			date: this.props.moment().format( 'YYYY-MM-DD' ),
+			quantity: '30'
+		};
+	}
 	render() {
+		const { siteId, unit, slug } = this.props;
 		return (
 			<Main className="woocommerce stats" wideLayout={ true }>
-				<StatsNavigation { ...this.props } />
+				{ siteId && <QuerySiteStats
+					siteId={ siteId }
+					statType="statsOrders"
+					query={ this.createQuery( unit ) }
+				/> }
+				<StatsNavigation unit={ unit } slug={ slug } type="orders" />
 			</Main>
 		);
 	}
 }
+
+const localizedStats = localize( Stats );
+
+export default connect(
+	state => {
+		return {
+			siteId: getSelectedSiteId( state ),
+			slug: getSelectedSiteSlug( state ),
+		};
+	}
+)( localizedStats );
--- client/extensions/woocommerce/app/stats/stats-navigation/index.js
+++ client/extensions/woocommerce/app/stats/stats-navigation/index.js
@@ -2,8 +2,7 @@
  * External Dependencies
  */
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
-import i18n from 'i18n-calypso';
+import { localize } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -13,26 +12,25 @@ import NavTabs from 'components/section-nav/tabs';
 import NavItem from 'components/section-nav/item';
 import FollowersCount from 'blocks/followers-count';
 import SegmentedControl from 'components/segmented-control';
-import { getSelectedSiteSlug } from 'state/ui/selectors';
 
 const StatsNavigation = props => {
-	const { translate, slug, type, period } = props;
-	const periods = {
+	const { translate, slug, type, unit } = props;
+	const units = {
 		day: translate( 'Days' ),
 		week: translate( 'Weeks' ),
 		month: translate( 'Months' ),
 		year: translate( 'Years' ),
 	};
 	return (
-		<SectionNav selectedText={ periods[ period ] }>
+		<SectionNav selectedText={ units[ unit ] }>
 			<NavTabs label={ translate( 'Stats' ) }>
-				{ Object.keys( periods ).map( key => (
+				{ Object.keys( units ).map( key => (
 					<NavItem
 						key={ key }
 						path={ `/store/stats/${ type }/${ key }/${ slug }` }
-						selected={ period === key }
+						selected={ unit === key }
 					>
-						{ periods[ key ] }
+						{ units[ key ] }
 					</NavItem>
 				) ) }
 			</NavTabs>
@@ -40,7 +38,7 @@ const StatsNavigation = props => {
 				className="stats-navigation__control"
 				initialSelected="store"
 				options={ [
-					{ value: 'site', label: translate( 'Site' ), path: `/stats/${ period }/${ slug }` },
+					{ value: 'site', label: translate( 'Site' ), path: `/stats/${ unit }/${ slug }` },
 					{ value: 'store', label: translate( 'Store' ) },
 				] }
 			/>
@@ -53,11 +51,4 @@ StatsNavigation.propTypes = {
 	slug: PropTypes.string
 };
 
-export default connect(
-	state => {
-		return {
-			slug: getSelectedSiteSlug( state ),
-			translate: i18n.translate,
-		};
-	}
-)( StatsNavigation );
+export default localize( StatsNavigation );
